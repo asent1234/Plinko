f8f7043281d04e3e957bbc07e0d8bbb847b68d67
asent1234 <42815464+asent1234@users.noreply.github.com>
2020-07-06 22:10:37 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -11,29 +11,18 @@ function setup() {
   particles = []
   canvasy = 800
   createCanvas(canvasx,canvasy);
-  //Particle1 = new Plinko(200,200)
+  Particle1 = new Plinko(200,200)
   ground = new Ground(canvasx/2,canvasy-10, width, 30, "white")
   divisionHeight = 300
-
-}
-
-function draw() {
-  Engine.update(engine);
-  background("black");
-  //Particle1.display();
-  ground.display();
   for(var k = 10; k <= width; k = k + 80){
     divisions.push(new Division(k, height-divisionHeight/2,10, divisionHeight, "white"))
-  }
-
-  for(var k = 0; k < divisions.length; k++){
-    divisions[k].display();
   }
   for(var j = 40; j <= width; j = j + 80){
     plinkos.push(new Plinko(j, 150))
     //console.log("hello")
   }
   for(var j = 15; j <= width; j = j + 80){
+    
     plinkos.push(new Plinko(j, 250))
     //console.log("hello")
   }
@@ -45,6 +34,19 @@ function draw() {
     plinkos.push(new Plinko(j, 450))
     //console.log("hello")
   }
+}
+
+function draw() {
+  Engine.update(engine);
+  background("black");
+  Particle1.display();
+  ground.display();
+  
+
+  for(var k = 0; k < divisions.length; k++){
+    divisions[k].display();
+  }
+
   if(frameCount%60 === 0){
     particles.push(new Particle(random(width/2 - 10, width/2 + 10), 10,10))
   }
